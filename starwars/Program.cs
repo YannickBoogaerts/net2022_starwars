65bba51c596ec8f934ec9f550b5a84135454f882
Yannick <yannick.boogaerts@technifutur.be>
2022-04-11 13:28:47 +0200

    Ajoutez des fichiers projet.

---
+++ starwars/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int choix = -1;
+String entree = "";
+
+Icommand[] exercices = new Icommand[]
+{
+};
+
+do
+{
+
+    for (int i = 0; i < exercices.Length; i++)
+    {
+        Console.WriteLine("{0,2} {1}", i + 1, exercices[i].Name);
+    }
+    Console.WriteLine("{0,2} {1}", 'q', "Quitter");
+    Console.WriteLine("--------------");
+    Console.Write("choix : ");
+    entree = Console.ReadLine();
+
+    if (Int32.TryParse(entree, out choix))
+    {
+        Console.Clear();
+        try
+        {
+            if (choix > 0 && choix <= exercices.Length)
+            {
+                exercices[choix - 1].Execute();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        Console.ReadKey();
+        Console.Clear();
+
+    }
+} while (entree != "q");
+
